4243f10c18a7907207f9174b49a372c67abb5d4e
myhackathons <myhackathons@gmail.com>
2013-10-16 21:06:34 -0700

    AdddedCategories

---
+++ LookupAPI/Controllers/CategoryController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using LookupAPI.Models;
+
+namespace LookupAPI.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        private LookupEntities db = new LookupEntities();
+
+        public class LookupCategory
+        {
+            public Guid  CategoryId { get; set; }
+            public string CategoryName { get; set; }
+            public string Icon { get; set; }
+        }
+
+        // GET api/Category
+        public List<LookupCategory> GetCategories()
+        {
+            List<LookupCategory> categories = new List<LookupCategory>();
+            foreach (var item in db.Categories.OrderBy(w=>w.Sequence))
+            {
+                LookupCategory a = new LookupCategory();
+                a.CategoryId = item.CategoryId;
+                a.CategoryName = item.CategoryName;
+                a.Icon = item.Icon;
+                categories.Add(a);
+            }
+            return categories;
+        }
+
+        //// GET api/Category/5
+        //public Category GetCategory(Guid id)
+        //{
+        //    Category category = db.Categories.Find(id);
+        //    if (category == null)
+        //    {
+        //        throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+        //    }
+
+        //    return category;
+        //}
+
+        //// PUT api/Category/5
+        //public HttpResponseMessage PutCategory(Guid id, Category category)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+        //    }
+
+        //    if (id != category.CategoryId)
+        //    {
+        //        return Request.CreateResponse(HttpStatusCode.BadRequest);
+        //    }
+
+        //    db.Entry(category).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //    }
+        //    catch (DbUpdateConcurrencyException ex)
+        //    {
+        //        return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+        //    }
+
+        //    return Request.CreateResponse(HttpStatusCode.OK);
+        //}
+
+        //// POST api/Category
+        //public HttpResponseMessage PostCategory(Category category)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Categories.Add(category);
+        //        db.SaveChanges();
+
+        //        HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, category);
+        //        response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = category.CategoryId }));
+        //        return response;
+        //    }
+        //    else
+        //    {
+        //        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+        //    }
+        //}
+
+        //// DELETE api/Category/5
+        //public HttpResponseMessage DeleteCategory(Guid id)
+        //{
+        //    Category category = db.Categories.Find(id);
+        //    if (category == null)
+        //    {
+        //        return Request.CreateResponse(HttpStatusCode.NotFound);
+        //    }
+
+        //    db.Categories.Remove(category);
+
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //    }
+        //    catch (DbUpdateConcurrencyException ex)
+        //    {
+        //        return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+        //    }
+
+        //    return Request.CreateResponse(HttpStatusCode.OK, category);
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
--- LookupAPI/Controllers/ContactController.cs
+++ LookupAPI/Controllers/ContactController.cs
@@ -115,73 +115,73 @@ namespace LookupAPI.Controllers
             return contact;
         }
 
-        // PUT api/Contact/5
-        public HttpResponseMessage PutContact(Guid id, Contact contact)
-        {
-            if (!ModelState.IsValid)
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-            }
+        ////// PUT api/Contact/5
+        ////public HttpResponseMessage PutContact(Guid id, Contact contact)
+        ////{
+        ////    if (!ModelState.IsValid)
+        ////    {
+        ////        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+        ////    }
 
-            if (id != contact.ContactId)
-            {
-                return Request.CreateResponse(HttpStatusCode.BadRequest);
-            }
+        ////    if (id != contact.ContactId)
+        ////    {
+        ////        return Request.CreateResponse(HttpStatusCode.BadRequest);
+        ////    }
 
-            db.Entry(contact).State = EntityState.Modified;
+        ////    db.Entry(contact).State = EntityState.Modified;
 
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
-            }
+        ////    try
+        ////    {
+        ////        db.SaveChanges();
+        ////    }
+        ////    catch (DbUpdateConcurrencyException ex)
+        ////    {
+        ////        return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+        ////    }
 
-            return Request.CreateResponse(HttpStatusCode.OK);
-        }
+        ////    return Request.CreateResponse(HttpStatusCode.OK);
+        ////}
 
-        // POST api/Contact
-        public HttpResponseMessage PostContact(Contact contact)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Contacts.Add(contact);
-                db.SaveChanges();
+        ////// POST api/Contact
+        ////public HttpResponseMessage PostContact(Contact contact)
+        ////{
+        ////    if (ModelState.IsValid)
+        ////    {
+        ////        db.Contacts.Add(contact);
+        ////        db.SaveChanges();
 
-                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, contact);
-                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = contact.ContactId }));
-                return response;
-            }
-            else
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-            }
-        }
+        ////        HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, contact);
+        ////        response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = contact.ContactId }));
+        ////        return response;
+        ////    }
+        ////    else
+        ////    {
+        ////        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+        ////    }
+        ////}
 
-        // DELETE api/Contact/5
-        public HttpResponseMessage DeleteContact(Guid id)
-        {
-            Contact contact = db.Contacts.Find(id);
-            if (contact == null)
-            {
-                return Request.CreateResponse(HttpStatusCode.NotFound);
-            }
+        ////// DELETE api/Contact/5
+        ////public HttpResponseMessage DeleteContact(Guid id)
+        ////{
+        ////    Contact contact = db.Contacts.Find(id);
+        ////    if (contact == null)
+        ////    {
+        ////        return Request.CreateResponse(HttpStatusCode.NotFound);
+        ////    }
 
-            db.Contacts.Remove(contact);
+        ////    db.Contacts.Remove(contact);
 
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
-            }
+        ////    try
+        ////    {
+        ////        db.SaveChanges();
+        ////    }
+        ////    catch (DbUpdateConcurrencyException ex)
+        ////    {
+        ////        return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+        ////    }
 
-            return Request.CreateResponse(HttpStatusCode.OK, contact);
-        }
+        ////    return Request.CreateResponse(HttpStatusCode.OK, contact);
+        ////}
 
         protected override void Dispose(bool disposing)
         {
